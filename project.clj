ced7362ac108769b2748d1ef5e7506890fdb292e
Pavel Kopychenko <t34box@gmail.com>
2024-05-04 13:52:37 +0300

    Upgraded library versions

--- project.clj
+++ project.clj
@@ -3,27 +3,26 @@
   :url "https://clojure.org"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/clojurescript "1.10.597"]
-                 [compojure "1.6.2"]
-                 [com.taoensso/timbre "5.2.1"]
+  :dependencies [[org.clojure/clojure "1.11.3"]
+                 [org.clojure/clojurescript "1.11.132"]
+                 [cheshire "5.13.0"]
+                 [compojure "1.7.1"]
+                 [com.taoensso/timbre "6.5.0"]
                  [ring/ring-json "0.5.1"]
-                 [reagent "0.10.0"]
-                 [re-frame "0.12.0"]
+                 [cljsjs/react "17.0.2-0"]
+                 [cljsjs/react-dom "17.0.2-0"]
+                 [reagent "1.2.0"]
+                 [re-frame "1.4.3"]
                  [cljs-ajax "0.8.4"]
                  [clj-http "3.12.3"]]
 
   :jvm-opts ^:replace ["-Dfile.encoding=UTF-8"]
 
-  :plugins [[lein-cljsbuild "1.1.8"]
-            [lein-ring "0.12.5"]
-            [lein-figwheel "0.5.19"]]
+  :plugins [[lein-ring "0.12.6"]
+            [lein-cljsbuild "1.1.8" :exclusions [[org.clojure/clojure]]]
+            [lein-figwheel "0.5.20"]]
 
-  :ring {:handler       challenge.handler/app
-         :auto-reload?  false
-         :auto-refresh? false
-         :uberwar-name  "challenge.war"
-         :port          3000}
+
 
   :auto-clean false
   :clean-targets ^{:protect false} ["resources/public/js"
@@ -35,10 +34,17 @@
                  "src/clj"
                  "src/cljs"]
 
+  :ring {:handler       challenge.handler/app
+         :auto-reload?  false
+         :auto-refresh? false
+         :uberwar-name  "challenge.war"
+         :port          3000}
+
   :figwheel {:ring-handler challenge.handler/app
              :nrepl-port   7888}
 
-  :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.19"]]}
+  :profiles {:dev {:dependencies [[binaryage/devtools "1.0.0"]
+                                  [figwheel-sidecar "0.5.20"]]}
 
              :uberwar {:aot         :all
                        :omit-source true}
